fed620d924cd1992d4bd7b3efe87fe04a7744cd5
Dario Spina <darspn@gmail.com>
2021-04-23 19:50:42 +0100

    readme and seed

--- README.md
+++ README.md
@@ -1,24 +1,13 @@
-# README
+In order to install the app run the following commands:
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+- `bundle install`
+- `rails db:create db:migrate db:seed`
+- `rails s -p 3000`
+  The routes for the api are:
 
-Things you may want to cover:
+POST /api/v1/campaigns/:id/create_investment
+GET /api/v1/campaigns
+GET /api/v1/campaigns/:id
 
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+They can be tested though Postman
+run `rspec spec` in order to run tests
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-c = Country.create(name: 'Great Britain', code: 'GB')
-s = Sector.create(name: 'Crypto', code: 'CRPT')
-camp = Campaign.create(name: 'Crypterium',
-                       image_source: 'shorturl.at/klmGK',
-                       percentage_raised: 0,
-                       target_amount_in_cents: 100_000_700,
-                       minimum_investment_in_cents: 1157,
-                       country: c,
-                       sector: s)
+Campaign.destroy_all
+Country.destroy_all
+Sector.destroy_all
+Investment.destroy_all
+c1 = Country.create(name: 'Great Britain', code: 'GB')
+c2 = Country.create(name: 'France', code: 'FR')
+c3 = Country.create(name: 'Italy', code: 'IT')
+countries = [c1, c2, c3]
+s1 = Sector.create(name: 'Crypto', code: 'CRPT')
+s2 = Sector.create(name: 'Tech', code: 'TECH')
+s3 = Sector.create(name: 'Magic', code: 'MAGIC')
+sectors = [s1, s2, s3]
 
-Investment.create(amount_in_cents: 1157, campaign: camp)
+company_names = %w[board lavish
+               flash
+               pin
+               thundering
+               guess
+               flawless
+               oatmeal
+               malicious
+               wholesale
+               knock
+               low
+               reaction
+               dock
+               wilderness
+               detailed
+               whimsical
+               analyze
+               materialistic
+               stomach]
+target_amounts = [100_000_700, 10_000_000, 10_000, 999, 4_556_565, 2_322_443, 54_464, 232_323, 245_232_434, 2_233_256, 232_323, 1_212_466, 7544]
+minimum_amounts =[1111,2222,3333,4000,5000,6000,7000,9000,10000]
+company_names.each do |cn|
+  Campaign.create(name: cn,
+                  image_source: 'shorturl.at/klmGK',
+                  percentage_raised: 0,
+                  target_amount_in_cents: target_amounts.sample,
+                  minimum_investment_in_cents: minimum_amounts.sample,
+                  country: countries.sample,
+                  sector: sectors.sample)
+end
+
+
+Investment.create(amount_in_cents: 1157, campaign: Campaign.last)
